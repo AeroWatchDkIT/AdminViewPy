f58ebcc5d86b4b8f230aacea083ea705ab0b1128
kylem <kyle.mcquillan.01@gmail.com>
2024-04-06 16:58:57 +0100

    :art: Added User Pallet Graphing

--- static/bootstrap/js/demo/PieChartUser.js
+++ static/bootstrap/js/demo/PieChartUser.js
@@ -1,44 +1,44 @@
-// Function to create the Pie Chart
-function createPieChart(data) {
-  var forkliftsWithUserId = data.entities.filter(function (forklift) {
-    return forklift.lastUserId !== null;
-  });
-
-  var forkliftsWithoutUserId = data.entities.filter(function (forklift) {
-    return forklift.lastUserId === null;
-  });
-
+// Assuming you have already included Chart.js in your project
+function createPieChart(correctPlacements, incorrectPlacements) {
   var ctx = document.getElementById("forkliftPieChart");
   var forkliftPieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: ["Correct Placements", "Misplaced Pallets"],
-      datasets: [{
-        data: [10, 20],
-        backgroundColor: ['#4e73df', '#1cc88a'],
-        hoverBackgroundColor: ['#2e59d9', '#17a673'],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
+      type: 'pie',
+      data: {
+          labels: ["Incorrect Placements", "Correct Pallets"],
+          datasets: [{
+              data: [correctPlacements, incorrectPlacements],
+              backgroundColor: ['#4e73df', '#1cc88a'],
+              hoverBackgroundColor: ['#2e59d9', '#17a673'],
+              hoverBorderColor: "rgba(234, 236, 244, 1)",
+          }],
       },
-      legend: {
-        display: true,
-        position: 'bottom',
+      options: {
+          maintainAspectRatio: false,
+          tooltips: {
+              backgroundColor: "rgb(255,255,255)",
+              bodyFontColor: "#858796",
+              borderColor: '#dddfeb',
+              borderWidth: 1,
+              xPadding: 15,
+              yPadding: 15,
+              displayColors: false,
+              caretPadding: 10,
+          },
+          legend: {
+              display: true,
+              position: 'bottom',
+          },
       },
-    },
   });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const chartPieDiv = document.querySelector('.chart-pie');
+  const correctPlacement = parseInt(chartPieDiv.getAttribute('data-correct-pallet-placement'), 10);
+  const incorrectPlacement = parseInt(chartPieDiv.getAttribute('data-incorrect-pallet-placement'), 10);
+
+  createPieChart(correctPlacement, incorrectPlacement);
+});
 
 // Fetch data from the API
 fetch('https://localhost:7128/Forklifts')
