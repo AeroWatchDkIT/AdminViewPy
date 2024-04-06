45884869043f490e82ae1c5bbfcaf480a831e06d
kylem <kyle.mcquillan.01@gmail.com>
2024-04-06 16:41:27 +0100

    Added

---
+++ static/bootstrap/js/demo/PieChartUser.js
@@ -0,0 +1,57 @@
+// Function to create the Pie Chart
+function createPieChart(data) {
+  var forkliftsWithUserId = data.entities.filter(function (forklift) {
+    return forklift.lastUserId !== null;
+  });
+
+  var forkliftsWithoutUserId = data.entities.filter(function (forklift) {
+    return forklift.lastUserId === null;
+  });
+
+  var ctx = document.getElementById("forkliftPieChart");
+  var forkliftPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ["Correct Placements", "Misplaced Pallets"],
+      datasets: [{
+        data: [10, 20],
+        backgroundColor: ['#4e73df', '#1cc88a'],
+        hoverBackgroundColor: ['#2e59d9', '#17a673'],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      }],
+    },
+    options: {
+      maintainAspectRatio: false,
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+    },
+  });
+}
+
+// Fetch data from the API
+fetch('https://localhost:7128/Forklifts')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Call the function to create the Pie Chart with the fetched data
+    createPieChart(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data from the API:', error);
+  });
